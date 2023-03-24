92b1c0f7699801a0082dc98a337e205fa672d294
moahammadxxali <ali_mohammad@t-online.de>
2023-03-25 00:27:17 +0100

    add js function countDown

--- app.js
+++ app.js
@@ -47,3 +47,32 @@ document.querySelectorAll('.small-image-3').forEach(images =>{
         document.querySelector('.big-image-3').src = images.getAttribute('src');
     }
 });
+
+
+let countDate = new Date('aug 1, 2021 00:00:00').getTime();
+
+function countDown(){
+
+    let now = new Date().getTime();
+	gap = countDate - now;
+
+    let seconds = 1000;
+    let minutes = seconds * 60;
+    let hours = minutes * 60;
+    let days = hours * 24;
+
+    let d = Math.floor(gap / (days));
+	let h = Math.floor((gap % (days)) / (hours));
+	let m = Math.floor((gap % (hours)) / (minutes));
+	let s = Math.floor((gap % (minutes)) / (seconds));
+
+    document.getElementById('days').innerText = d;
+    document.getElementById('hours').innerText = h;
+    document.getElementById('minutes').innerText = m;
+    document.getElementById('seconds').innerText = s;
+
+}
+
+setInterval(function(){
+    countDown()
+},1000);
