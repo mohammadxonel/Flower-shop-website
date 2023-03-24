f74279a8966af9c0f63f10dbbc71c2775230d858
moahammadxxali <ali_mohammad@t-online.de>
2023-03-24 23:59:03 +0100

    add js edits to theme-toggler and images

--- app.js
+++ app.js
@@ -16,5 +16,34 @@ window.onscroll = () =>{
     }else{
         document.querySelector('header').classList.remove('active');
     }
-    
+
 }
+
+let themeToggler = document.querySelector('#theme-toggler');
+
+themeToggler.onclick = () =>{
+    themeToggler.classList.toggle('fa-sun');
+    if(themeToggler.classList.contains('fa-sun')){
+        document.querySelector('body').classList.add('active');
+    }else{
+        document.querySelector('body').classList.remove('active');
+    }
+}
+
+document.querySelectorAll('.small-image-1').forEach(images =>{
+    images.onclick = () =>{
+        document.querySelector('.big-image-1').src = images.getAttribute('src');
+    }
+});
+
+document.querySelectorAll('.small-image-2').forEach(images =>{
+    images.onclick = () =>{
+        document.querySelector('.big-image-2').src = images.getAttribute('src');
+    }
+});
+
+document.querySelectorAll('.small-image-3').forEach(images =>{
+    images.onclick = () =>{
+        document.querySelector('.big-image-3').src = images.getAttribute('src');
+    }
+});
